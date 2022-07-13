bb8ce7c29ca9775690b5842e0bac3a7411fb601f
19UV <the19UV@gmail.com>
2022-07-12 21:20:47 -0400

    Added helper functions for random number generation

---
+++ include/util/random.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cglm/cglm.h>
+
+void init_random(void);
+
+float float_random(void);
+void vec4_random(vec4 vector);
+
+void unit_sphere_4d_random(vec4 vector);
---
+++ src/util/random.c
@@ -0,0 +1,26 @@
+#include "util/random.h"
+
+#include <stdlib.h>
+#include <time.h>
+
+void init_random(void) {
+	time_t t;
+
+	srand((unsigned)time(&t));
+}
+
+float float_random(void) {
+	return (float)rand() / (float)RAND_MAX;
+}
+
+void vec4_random(vec4 vector) {
+	vector[0] = float_random();
+	vector[1] = float_random();
+	vector[2] = float_random();
+	vector[3] = float_random();
+}
+
+void unit_sphere_4d_random(vec4 vector) {
+	vec4_random(vector);
+	glm_vec4_normalize(vector);
+}
